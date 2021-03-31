920d42c905d80bbda0d6099dc92ca6051b8029a4
Phil Bailey <philbailey@msn.com>
2021-03-30 21:10:22 -0400

    add part 6

--- models/reservation.js
+++ models/reservation.js
@@ -4,11 +4,10 @@ const moment = require("moment");
 
 const db = require("../db");
 
-
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -19,26 +18,50 @@ class Reservation {
   /** formatter for startAt */
 
   getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+    return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
   }
 
   /** given a customer id, find their reservations. */
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
-    return results.rows.map(row => new Reservation(row));
+    return results.rows.map((row) => new Reservation(row));
+  }
+
+  /** save this reservation. */
+
+  async save() {
+    console.log(this.customerId);
+    console.log(this.numGuests);
+    console.log(this.startAt);
+    console.log(this.notes);
+
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id,start_at,num_guests,notes)
+               VALUES ($1, $2, $3, $4)
+               RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET customer_id=$1, start_at=$2, num_guests=$3, notes=$4
+               WHERE id=$5`,
+        [this.customerId, this.startAt, this.numGuests, this.notes, this.id]
+      );
+    }
   }
 }
-
 
 module.exports = Reservation;
